1bb7ea0d96336f2f9a108ee239db89f45f71a3ee
phoenixx776 <77883981+phoenixx776@users.noreply.github.com>
2021-04-28 11:24:09 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Rockstar
-Rockstar
+# AngelBOTV2
---
+++ bot.js
@@ -0,0 +1,158 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const { Client, Util } = require('discord.js');
+require('./util/eventLoader.js')(client);
+const fs = require('fs');
+const  db  = require('wio.db')
+
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+    console.log(`${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./komutlar/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
+});
+
+
+
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+    client.on("guildMemberAdd", async member => {
+        let sayac = db.get(`sayac.${member.guild.id}`);
+        if (!sayac) return;
+        let kanal = client.channels.get(sayac.kanal);
+        if (!kanal) return db.delete(`sayac.${member.guild.id}`);
+        kanal.send(`${member.user.tag} sunucuya katıldı! Sunucu şu an **${member.guild.memberCount}** kişi. **${sayac.sayi}** kişi olmamıza **${sayac.sayi - member.guild.memberCount}** kişi kaldı!`);
+        
+        if (member.guild.memberCount >= sayac.sayi) {
+          kanal.send(`Sunucu, sayaç hedefine ulaştı!`);
+          db.delete(`sayac.${member.guild.id}`);
+        };
+      });
+      
+      client.on("guildMemberRemove", async member => {
+        let sayac = db.get(`sayac.${member.guild.id}`);
+        if (!sayac) return;
+        let kanal = client.channels.get(sayac.kanal);
+        if (!kanal) return db.delete(`sayac.${member.guild.id}`);
+        kanal.send(`${member.user.tag} sunucudan ayrıldı! Sunucu şu an **${member.guild.memberCount}** kişi. **${sayac.sayi}** kişi olmamıza **${sayac.sayi - member.guild.memberCount}** kişi kaldı!`);
+      });      
+
+
+
+
+};
+
+
+
+
+client.on('message', message =>{
+    const sa = message.content.toLowerCase()
+    
+    if(sa === 'sa' || sa === 'sea' || sa === 'selam aleyküm' || sa === 'Selam Aleyküm') {
+    message.channel.send(`Aleyküm Selam Hoş Geldin <@${message.author.id}>`)
+    }
+    })
+
+client.on("ready", async () => {
+    log("Durum başarıyla ayarlandı")
+       client.user.setActivity("🤔 /yardım", 
+         { url: 'https://www.twitch.tv/.',
+         type: 'STREAMING' }); 
+        })
+ 
+
+
+
+ 
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+    let permlvl = 0;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on('warn', e => {
+    console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+    console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(ayarlar.token);
